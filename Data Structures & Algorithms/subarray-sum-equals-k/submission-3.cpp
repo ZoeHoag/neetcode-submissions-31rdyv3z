1445a406ba7a6aaf4386b27e98e6476478b2c04c
huy <69897697+ZoeHoag@users.noreply.github.com>
2026-05-20 00:43:38 +0700

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> store;
+        store[0] = 1;
+        int total = 0;
+        int res = 0;
+        for(int &num: nums) {
+            total += num;
+            res += store[total - k];
+            ++store[total];
+        }
+        return res;
+    }
+
+    // 0 2  1 2 4
+    // 0 8 16, 24, 15 17
+};
