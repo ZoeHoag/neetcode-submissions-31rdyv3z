a65b20a8f884d14b32e47902d6a8caeef30c049c
huy <69897697+ZoeHoag@users.noreply.github.com>
2026-06-14 00:20:37 +0700

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,int> store;
+        for(string &wl:wordList) {
+            store[wl] = INT_MAX;
+        }
+        if(!store.contains(endWord)) return 0;
+        priority_queue<pair<int,string>> pending;
+        store[beginWord] = 1;
+        pending.emplace(1, beginWord);
+        while(!pending.empty()) {
+            pair<int,string> cur = pending.top();
+            pending.pop();
+            if(cur.first > store[cur.second]) continue;
+            for(int i = 0; i < cur.second.size(); ++i) {
+                string tmp = cur.second;
+                for(char c = 'a'; c <= 'z'; ++c) {
+                    tmp[i] = c;
+                    if(tmp == endWord) return cur.first + 1;
+                    if(store.contains(tmp) && store[tmp] > cur.first + 1) {
+                        store[tmp] = cur.first + 1;
+                        pending.emplace(cur.first + 1, tmp);
+                    }
+                }
+            }
+        }
+        return store[endWord] == INT_MAX ? 0 : store[endWord];
+    }
+};
